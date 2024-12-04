0c8ac565702bc93691df16a079bc03f142cba021
Sean <1963966513@qq.com>
2024-12-04 21:11:48 +0800

    feat: 添加ga

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,16 +1,24 @@
 module.exports = {
-    base: "/clipboard_keyboard/",
-    themeConfig: {
-      sidebar: [
-        '/',
-      ],
-      search: false,
-    },
-    markdown: {
-      lineNumbers: true
-    },
-    less: {},
-    chainWebpack (config) {
-        config.resolve.alias.set('core-js/library/fn', 'core-js/features');
-    },
-  }
+  base: "/clipboard_keyboard/",
+  themeConfig: {
+    sidebar: [
+      '/',
+    ],
+    search: false,
+  },
+  markdown: {
+    lineNumbers: true
+  },
+  less: {},
+  chainWebpack(config) {
+    config.resolve.alias.set('core-js/library/fn', 'core-js/features');
+  },
+  plugins: [
+    [
+      '@vuepress/google-analytics',
+      {
+        'ga': 'G-3LPK9KXQ75' // UA-00000000-0
+      }
+    ]
+  ]
+}
